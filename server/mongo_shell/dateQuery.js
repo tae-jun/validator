d58fde514372c111ddf5a0e1f96f3bc50261b521
tae-jun <i2r.jun@gmail.com>
2014-10-18 22:51:37 +0900

    Required files fixed

---
+++ server/mongo_shell/dateQuery.js
@@ -0,0 +1,7 @@
+﻿// 2014-10-08 ~ 2014-10-09
+db.log.find({
+    _id: {
+        $gt: new Date('2014-10-08').toISOString(),
+        $lt: new Date('2014-10-09').toISOString()
+    }
+});
---
+++ server/mongo_shell/dump.js
@@ -0,0 +1,46 @@
+﻿var logNum = 1000;
+
+db.log.drop();
+
+var logs = [];
+
+function ran(max) {
+    return Math.round(Math.random() * max);
+}
+
+for (var i = 0; i < logNum; i++) {
+    var now = new Date();
+    now.setTime(now.getTime() - ran(7 * 1000 * 60 * 60 * 24)); // 7 days before ~ now
+
+    var isError;
+    var isChecked;
+
+    if (ran(10) == 0) {
+        isError = true;
+        isChecked = false;
+    }
+    else {
+        isError = false;
+        isChecked = undefined;
+    }
+
+    var record = {
+        _id: now.toISOString(),
+        isError: isError,
+        isChecked: isChecked
+    };
+
+    if (record.isChecked == undefined)
+        delete record.isChecked;
+
+    logs.push(record);
+}
+
+db.log.insert(logs);
+
+db.log.find({
+    $and: [
+        { isError: true },
+        { isChecked: false }
+    ]
+}).count();
