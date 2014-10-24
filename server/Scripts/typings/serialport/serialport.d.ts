1b4bed21592c048e2409befacc0f9c4f93604dbb
tae-jun <i2r.jun@gmail.com>
2014-10-24 13:51:32 +0900

    VS close

--- server/Scripts/typings/serialport/serialport.d.ts
+++ server/Scripts/typings/serialport/serialport.d.ts
@@ -1,41 +1,56 @@
 ﻿declare module 'serialport' {
-
-    function list(callback: (err: Error, ports: any[]) => void): void;
+    /**
+     * List the ports along with some metadata as well.
+     */
+    function list(callback: (err: Error, ports: IPort[]) => void): void;
 
     /**
      * Create a new serial port on path
      */
     class SerialPort {
         constructor(path: string, options?: ISerialPortOption, openImmediately?: boolean, callback?: (err) => void);
-
+        /**
+         * Opens a connection to the given serial port.
+         */
+        open(callback?: (err: Error) => void): void;
+        /**
+         * Closes an open connection.
+         */
+        close(callback?: (err: Error) => void): void;
+        /**
+         * Writes data to the given serial port.
+         */
+        write(buffer: Buffer, callback?: (err: Error) => void): void;
         /**
          * set event listener
          */
-        on(evnetName: "open", listener: (data: any) => void): void;
-        on(evnetName: "data", listener: (data: any) => void): void;
-        on(evnetName: "close", listener: (data: any) => void): void;
-        on(evnetName: "error", listener: (data: any) => void): void;
+        on(evnetName: 'open', listener: (data: any) => void): void;
+        on(evnetName: 'data', listener: (data: any) => void): void;
+        on(evnetName: 'close', listener: (data: any) => void): void;
+        on(evnetName: 'error', listener: (data: any) => void): void;
         on(evnetName: string, listener: (data: any) => void): void;
-
-        write(buffer: Buffer, callback?: (err: Error) => void): void;
-
-        close(callback: (err: Error) => void): void;
     }
 
     interface ISerialPortOption {
-        baudRate: number;
-        dataBits: number;
-        stopBits: number;
-        parity: string;
+        baudRate?: number;
+        dataBits?: number;
+        stopBits?: number;
+        parity?: string;
         //rtscts
         //xon
         //xoff
         //xany
         //flowControl
-        bufferSize: number;
+        bufferSize?: number;
         //parser
         //encoding
         //dataCallback
         //disconnectedCallback
+    }
+
+    interface IPort {
+        comName: string;
+        pnpId: string;
+        manufacturer: string;
     }
 }
--- server/src/serial/logger.ts
+++ server/src/serial/logger.ts
@@ -3,17 +3,35 @@ import SerialPort = serialport.SerialPort;
 
 var ports: SerialPort[] = [];
 
+/**
+ * Start logging.
+ */
 export function start() {
+    // Search every ports available
     serialport.list((err, portInfos) => {
         if (err) return console.dir(err);
 
         console.log(portInfos.length + ' ports detected');
 
         portInfos.forEach((portInfo) => {
-            //new SerialPort(portInfo.comName);
-            
+            // Create new serial port. Doesn't open immediately.
+            var port = new SerialPort(portInfo.comName, {/*options*/}, false);
+            // Open port
+            port.open((err) => {
+                if (err) return console.dir(err);
+                // Allocates a new buffer using an array of octets.
+                // octet is (8bit) byte
+                var buffer = new Buffer([0, 0, 0]);
+
+                port.write(buffer);
+                port.on('data', (data) => {
+                    var correctRes = '';
+                    if (data == correctRes)
+                        ports.push(port);
+                });
+            });
         });
-        
+
         onOpen();
     });
 
