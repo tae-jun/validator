2cd2e3db8da9b2cc7fe3fd54447976f4c67a226d
tae-jun <i2r.jun@gmail.com>
2015-01-07 18:52:28 +0900

    Build systen

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -1,39 +1,47 @@
-/// <vs />
+/*global module:false*/
 module.exports = function (grunt) {
+
     // These plugins provide necessary tasks.
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-ts');
 
     // Default task.
-    grunt.registerTask('default', ['clean', 'concat', 'copy']);
+    grunt.registerTask('default', ['ts', 'clean', 'concat', 'copy']);
+    grunt.registerTask('dev', ['default', 'watch']);
 
     // Project configuration.
     grunt.initConfig({
         // Metadata.
         pkg: grunt.file.readJSON('package.json'),
+        distFolder: 'dist',
+        tplFolder: 'dist/tpl',
         src: {
-            tsout: 'src/tsout.js',
-            css: 'src/**/*.css',
-            tpl: 'src/**/*.tpl.html',
-            index: 'src/index.html'
+            index: ['src/index.html'],
+            tpl: ['src/**/*.tpl.html'],
+            css: ['src/**/*.css'],
+            ts: ['src/**/*.ts'],
+            tsout: ['tsout/*.js'],
+            tsoutmap: ['tsout/*.js.map'],
+            gruntFile: ['Gruntfile.js']
         },
-        dist: 'dist',
         // Task configuration.
-        clean: ['<%= dist %>/**/*'],
+        clean: ['<%= distFolder %>/**/*'],
         concat: {
+            css: {
+                src: ['<%= src.css %>', 'vendor/**/*.css'],
+                dest: '<%= distFolder %>/<%= pkg.name %>.css'
+            },
             // because of process option, must concat. Do not just copy
             index: {
                 src: ['<%= src.index %>'],
-                dest: '<%= dist %>/index.html',
+                dest: '<%= distFolder %>/index.html',
                 options: {
                     process: true
                 }
-            },
-            css: {
-                src: ['<%= src.css %>', 'lib/**/*.css', '!**/*.min.css'],   // exclude *.min.css files
-                dest: '<%= dist %>/<%= pkg.name %>.css'
             }
         },
         copy: {
@@ -41,35 +49,71 @@ module.exports = function (grunt) {
                 files: [
                     {
                         expand: true,
-                        cwd: 'src',
-                        src: '**/*.tpl.html',
-                        dest: '<%= dist %>',
                         flatten: true,
+                        src: ['<%= src.tpl %>'],
+                        dest: '<%= tplFolder %>',
                         filter: 'isFile'
                     }
                 ]
             },
             tsout: {
                 src: '<%= src.tsout %>',
-                dest: '<%= dist %>/<%= pkg.name %>.js'
+                dest: '<%= distFolder %>/<%= pkg.name %>.js'
+            }
+        },
+        uglify: {
+            options: {
+                banner: '<%= banner %>'
             },
-            lib: {
-                files: [
-                    {
-                        expand: true,
-                        cwd: 'bower_components',
-                        src: ['*/dist/**/*.css', '*/dist/**/*.js', '!**/*.min.*', 'angular/angular.js'],
-                        dest: '<%= dist %>',
-                        flatten: true,
-                        filter: 'isFile'
-                    }
-                ]
+            dist: {
+                src: '<%= concat.dist.dest %>',
+                dest: 'dist/<%= pkg.name %>.min.js'
             }
         },
         watch: {
-            src: {
-                files: ['<%= src.tsout %>', '<%= src.css %>', '<%= src.tpl %>', '<%= src.index %>'],
-                tasks: ['default']
+            sources: {
+                files: ['<%= src.index %>', '<%= src.tpl %>', '<%= src.css %>', '<%= src.tsout %>'],
+                tasks: ['clean', 'concat', 'copy'],
+                options: {
+                    livereload: true
+                }
+            },
+            // Reload grunt config when Gruntfile.js is changed
+            gruntFile: {
+                files: ['<%= src.gruntFile %>'],
+                options: {
+                    reload: true
+                }
+            }
+        },
+        ts: {
+            // A specific target
+            build: {
+                // The source TypeScript files, http://gruntjs.com/configuring-tasks#files
+                src: ['<%= src.ts %>'],
+                // The source html files, https://github.com/grunt-ts/grunt-ts#html-2-typescript-support
+                //html: ["test/work/**/*.tpl.html"],
+                // If specified, generate this file that to can use for reference management
+                //reference: "./test/reference.ts",
+                // If specified, generate an out.js file which is the merged js file
+                out: 'tsout/tsout.js',
+                // If specified, the generate JavaScript files are placed here. Only works if out is not specified
+                //outDir: 'test/outputdirectory',
+                // If specified, watches this directory for changes, and re-runs the current target
+                //watch: 'test',
+                // Use to override the default options, http://gruntjs.com/configuring-tasks#options
+                options: {
+                    // 'es3' (default) | 'es5'
+                    target: 'es5',
+                    // 'amd' (default) | 'commonjs'
+                    //module: 'commonjs',
+                    // true (default) | false
+                    sourceMap: false,
+                    // true | false (default)
+                    declaration: false,
+                    // true (default) | false
+                    removeComments: true
+                },
             }
         }
     });
